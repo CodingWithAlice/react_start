aae68aae6da5bc0ec955aeab801705b9792373eb
haoling <haoling@corp.netease.com>
2021-12-01 22:05:59 +0800

    add：React哲学 - 静态

--- src/index.js
+++ src/index.js
@@ -1,113 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 基础数据
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
-const scaleNames = {
-  'c': 'Celsius',
-  'f': 'Fahrenheit',
-};
-
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
+// 搜索栏
+function SearchBar({value, onChangeValue}) {
+  return (
+    <div>
+      <input type="text" placeholder='search...' value={value} onChange={onChangeValue} />
+      <div>
+        <input type='checkbox' width='30'/>
+        Only show products in stock
+      </div>
+    </div>
+  )
+}
+// 列表每一个分类的分类名称
+const ProductCategoryRow = ({category}) => {
+  return (
+    <tr>
+      <td>{category}</td>
+    </tr>
+  );
+}
+// 列表每一个分类的分类产品
+const ProductRow = ({product}) => {
+  const name = product.stocked ? (product.name) : (
+    <span style={{color: 'red'}}>{product.name}</span>
+  )
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  )
 }
 
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if(Number.isNaN(input)) {
-    return '';
-  }
-  const res = convert(input);
-  return Math.round(res * 1000) / 1000;
-}
-
-function BoilingVerdict(props) {
-  if(props.celsius >= 100) {
-    return <p>水会沸腾</p>
-  }
-  return <p>水不会沸腾</p>
-}
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
-  }
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-    return (
-      <fieldset>
-        <legend>
-          以{scaleNames[scale]}格式输入温度
-        </legend>
-        <input 
-          value={temperature}
-          onChange={this.handleChange}
+// 产品列表
+function ProductTable({products}) {
+  const rows = [];
+  let lastCategory;
+  
+  products.forEach((pro) => {
+    if(pro.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow 
+          category={pro.category}
+          key={pro.category}
         />
-      </fieldset>
+      );
+    }
+    rows.push(
+      <ProductRow 
+        product={pro}
+        key={pro.name}
+      />
     )
-  }
+    lastCategory = pro.category;
+  })
+  
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>价格</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
 }
 
-class Caculator extends React.Component {
+// 最外面的 outer
+class FilterableProductTable extends React.Component {
   constructor(props) {
     super(props);
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
     this.state = {
-      temperature: '',
-      scale: 'c',
+      searchWord: '',
+      checked: false,
     };
   }
 
-  handleCelsiusChange(temperature) {
-    this.setState({
-      temperature,
-      scale: 'c',
-    })
-  }
-
-  handleFahrenheitChange(temperature) {
-    this.setState({
-      temperature,
-      scale: 'f',
-    })
+  // 改变 input 的 value 值的方法
+  onChangeValue(newValue) {
+    this.state.searchWord = newValue;
   }
 
   render() {
-    const scale = this.state.scale;
-    const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius): temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit): temperature;
-
     return (
       <div>
-        <TemperatureInput
-          scale='c'
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange}
-        />
-        <TemperatureInput
-          scale='f'
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange}
-        />
-        <BoilingVerdict
-          celsius={parseFloat(celsius)}
-        />
+        <SearchBar
+          value={this.state.searchWord}
+          onChangeValue={this.onChangeValue}/>
+        <ProductTable products={PRODUCTS} />
       </div>
-    )
+    );
   }
 }
 
 ReactDOM.render(
-  <Caculator />,
+  <FilterableProductTable />,
   document.getElementById('root')
-);
+)
